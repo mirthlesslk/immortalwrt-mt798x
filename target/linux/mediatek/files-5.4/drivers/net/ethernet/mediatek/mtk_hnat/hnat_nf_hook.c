e623090a7c1f60e46de2744964ff91aae7a5c281
mirthlesslk <101487885+mirthlesslk@users.noreply.github.com>
2026-05-20 22:43:13 +0800

    Update hnat_nf_hook.c

--- target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat_nf_hook.c
+++ target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat_nf_hook.c
@@ -2179,10 +2179,8 @@ static unsigned int mtk_hnat_nf_post_routing(
 	if (!IS_LAN(out) && !IS_WAN(out) && !IS_EXT(out))
 		return 0;
 
-#if defined(CONFIG_MEDIATEK_NETSYS_RX_V2)
-	if (!IS_WHNAT(out) && IS_EXT(out) && FROM_WED(skb))
-		return 0;
-#endif
+	//if (!IS_WHNAT(out) && IS_EXT(out) && FROM_WED(skb))
+	//	return 0;
 
 	trace_printk("[%s] case hit, %x-->%s, reason=%x\n", __func__,
 		     skb_hnat_iface(skb), out->name, skb_hnat_reason(skb));
@@ -2571,4 +2569,3 @@ int mtk_hqos_ptype_cb(struct sk_buff *skb, struct net_device *dev,
 
 	return 0;
 }
-
